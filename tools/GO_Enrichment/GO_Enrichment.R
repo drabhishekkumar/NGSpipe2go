f960996756ec085189d9dc1c2bcb0005dd84ce71
ssayols <sergisayolspuig@gmail.com>
2020-10-29 16:27:16 +0100

    reduced representation of GO enriched terms

--- tools/GO_Enrichment/GO_Enrichment.R
+++ tools/GO_Enrichment/GO_Enrichment.R
@@ -26,6 +26,7 @@ library(clusterProfiler)
 library(ReactomePA)
 library(Cairo)
 library(ggplot2)
+library(rrvgo)
 
 # supported organisms
 orgDb <- c(human="org.Hs.eg.db",
@@ -123,9 +124,25 @@ processContrast <-  function(x) {
 
 
         if(!is.null(enriched) && nrow(enriched) > 0) {
+            # calculate reduced terms and generate reduced term plots and table
+            go_analysis  <- as.data.frame(enriched)
+            scores       <- setNames(-log10(go_analysis$qvalue), go_analysis$ID)
+            simMatrix    <- calculateSimMatrix(go_analysis$ID, orgdb=orgDb[org], ont="BP", method="Rel")
+            reducedTerms <- reduceSimMatrix(simMatrix, scores, threshold=0.7, orgdb=orgDb[org])
+            CairoPNG(file=paste0(out, "/", contrast, "_GO_scatterplot_", suffix, "_genes.png"), width=1200, height=800)
+            print(scatterPlot(simMatrix, reducedTerms))
+            dev.off()
+            CairoPNG(file=paste0(out, "/", contrast, "_GO_treemap_", suffix, "_genes.png"), width=1200, height=800)
+            treemapPlot(reducedTerms)
+            dev.off()
+            write.csv(reducedTerms, file=paste0(out, "/", contrast, "_GO_Enrichment_Reduced_", suffix, "_genes.csv"))
+
+            enriched_reduced <- enriched
+            enriched_reduced@result <- subset(enriched_reduced@result, ID %in% unique(reducedTerms$parent))
+
             # create barplot showing GO category
             CairoPNG(file=paste0(out, "/", contrast, "_GO_Barplot_", suffix, "_genes.png"), width=700, height=500)
-            print(barplot(enriched, showCategory=plotCategory) + ylab("Number of genes"))
+            print(barplot(enriched_reduced, showCategory=plotCategory) + ylab("Number of genes"))
             dev.off()
       
             # create network plot for the results
@@ -135,7 +152,7 @@ processContrast <-  function(x) {
 
             # create cnetplot for the results
             CairoPNG(file=paste0(out, "/", contrast, "_GO_cnetplot_", suffix, "_genes.png"), width=1200, height=800)
-            print(cnetplot(enriched, categorySize="pvalue", foldChange=entrezDeIdLfc))
+            print(cnetplot(enriched_reduced, categorySize="pvalue", foldChange=entrezDeIdLfc))
             dev.off()
         }
 
